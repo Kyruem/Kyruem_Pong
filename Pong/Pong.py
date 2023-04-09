59a60b4be231329d50039421367e7cd9cb13e5e9
Kyruem <129579032+Kyruem@users.noreply.github.com>
2023-04-09 09:18:12 +0700

    Add files via upload

---
+++ Pong/Pong.py
@@ -0,0 +1,92 @@
+from pygame import *
+font.init()
+
+# region window
+WIDTH, HEIGHT = 1000, 725
+window = display.set_mode((WIDTH, HEIGHT))
+clock = time.Clock()
+# endregion window
+
+# region Classes
+class ImageSprite(sprite.Sprite):
+    # constructor function. Runs ONCE every time a new object it's created
+    def __init__(self, filename, pos, size):
+        super().__init__()
+        self.image = image.load(filename)
+        self.image = transform.scale(self.image, size)
+        self.rect = Rect(pos, size)
+        self.initial_pos = pos
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+
+    def reset(self):
+        self.rect.topleft = self.initial_pos
+
+class PlayerSprite(ImageSprite):
+    def update1(self):
+        keys = key.get_pressed()
+        if keys[K_s]:
+            self.rect.y += 7
+        if keys[K_w]:
+            self.rect.y -= 7
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_DOWN]:
+            self.rect.y += 7
+        if keys[K_UP]:
+            self.rect.y -= 7
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+class BallSprite(ImageSprite):
+    def __init__(self, filename, pos, size, speed):
+        super().__init__(filename, pos, size)
+        self.speed = Vector2(speed)
+    
+    def update(self):
+        self.rect.topleft += self.speed
+    
+    def VerticalBounce(self):
+        self.speed.y *= -1
+    
+    def HorizontalBounce(self):
+        self.speed.x *= -1
+
+    def collision(self, other_sprite):
+        col = sprite.collide_rect(self,other_sprite)
+        return col
+        
+
+# region Sprite
+p1 = PlayerSprite(filename="VengefulSpirit.png", pos=(0,250), size=(115,70))
+p2 = PlayerSprite(filename="ShadeSoul.png", pos=(885,250), size=(115,70))
+ball = BallSprite(filename="Dreamball.png", pos=(300,350), size=(70,70), speed=(2,4))
+
+while not event.peek(QUIT):
+    window.fill("black")
+    
+    p1.draw(window)
+    p1.update1()
+    p2.draw(window)
+    p2.update2()
+    
+    if ball.rect.top < 0 or ball.rect.bottom > HEIGHT:
+        ball.VerticalBounce()
+
+    # if ball.rect.left < 0 or ball.rect.right > WIDTH:
+
+    ball.draw(window)
+    ball.update()
+
+    display.update()
+    clock.tick(100)
